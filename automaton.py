b7aa3a8d0e8972f712e145fdca14286e875e7990
Gabriel Briones <gabriel.briones.sayeg@intel.com>
2020-11-21 10:04:15 -0600

    Union and intersect operations
    Signed-off-by: Gabriel Briones <gabriel.briones.sayeg@intel.com>

--- automaton.py
+++ automaton.py
@@ -1,6 +1,34 @@
 import enum
 
 EPSILON_SYMBOL = "$"
+
+class State():
+
+    def __init__(self, name):
+        self.name = name
+        self.transitions = set()
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            return NotImplemented
+        return self.name == other.name
+
+    def __lt__(self, other):
+        if not isinstance(other, type(self)):
+            return NotImplemented
+        return self.name < other.name
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+    def add_transition(self, target: str, symbol: str):
+        self.transitions.add(Transition(self.name, target, symbol))
 class Transition():
 
     def __init__(self, origin: str, target: str, symbol: str):
@@ -8,7 +36,7 @@ class Transition():
         self.target = target
         self.symbol = symbol
 
-    def render(self):
+    def render(self) -> str:
         return f"{self.origin}:{self.symbol}>{self.target}"
 
     def __str__(self):
@@ -26,49 +54,82 @@ class operationID(enum.Enum):
 
 class Automaton():
 
-    def __init__(self, from_file):
-        self.states = []
-        self.initial = ""
-        self.accept = []
-        self.alphabet = []
-        self.transitions = []
-        self.is_valid = False
+    def __init__(self, states=None, initial=None, accept=None, alphabet=None, from_file=None):
+        self.states = states
+        self.initial = initial
+        self.accept = accept
+        self.alphabet = alphabet
+        self._transitions = []
         self.errors = []
-        self._parse_file(from_file)
-        self.validate()
-        import pdb; pdb.set_trace()
+        if from_file:
+            self.states = set()
+            self.initial = ""
+            self.accept = set()
+            self.alphabet = set()
+            self._parse_file(from_file)
+        else:
+            self._parse_transitions_from_states()
+        self.is_valid = self.validate()
 
-    def validate(self):
-        # TODO: Validate if automaton is valid, if so, set self.is_valid = True
-        # if not, add errors to self.errors
-        
-        #"set" help to see if an element is repeated (if lenght is different from the lenght of the set, this means a element is repeated)
-        #states repeated, throw an error
-        if len(self.states) != len(set(self.states)):
-            self.errors.append("States repeated")
+    def validate(self) -> bool:
         #initial state is undefined
-        if self.initial == "" or self.initial not in self.states:
-            self.errors.append("No initial state")
+        if self.initial == "":
+            self.errors.append("Initial state not defined")
+        if State(self.initial) not in self.states:
+            self.errors.append("Invalid initial state")
         #accept state has to exist in states
         for sa in self.accept:
-            if sa not in self.states:
+            if State(sa) not in self.states:
                 self.errors.append("Invalid initial state")
-        #alphabet cannot be repeated
-        if len(self.alphabet) != len(set(self.alphabet)):
-            self.errors.append("Alphabet repeated")
         #validate if states of transitions are valid
-        for trans in self.transitions:
-            if trans.origin not in self.states:
+        for trans in self._transitions:
+            origin = self.get_state(trans.origin)
+            target = self.get_state(trans.target)
+            if origin is None:
                 self.errors.append(f"Transition has invalid origin state {trans.origin}")
-            if trans.target not in self.states:
+            elif target is None:
                 self.errors.append(f"Transition has invalid target state {trans.target}")
-            if trans.symbol != EPSILON_SYMBOL and trans.symbol not in self.alphabet:
+            elif trans.symbol != EPSILON_SYMBOL and trans.symbol not in self.alphabet:
                 self.errors.append(f"Transition has undefined symbol in alphabet: {trans.symbol}")
-                    
+            else:
+                origin.add_transition(target.name, trans.symbol)
+        # TODO: Remove unreachable states
+        if not self.errors:
+            return True
+        return False
 
-    def render(self):
-        # TODO: Return a string in a format accepted by FSM simulator
-        pass
+    def get_state(self, state_name) -> State:
+        for state in self.states:
+            if state.name == state_name:
+                return state
+        return None
+
+    def get_states_name(self) -> list:
+        names = []
+        for state in sorted(self.states):
+            names.append(state.name)
+        return names
+
+    def _parse_transitions_from_states(self) -> list:
+        for state in sorted(self.states):
+            for trans in state.transitions:
+                self._transitions.append(trans)
+
+    def render(self) -> str:
+        output = "#states\n"
+        for state in sorted(self.states):
+            output += f"{state}\n"
+        output += f"#initial\n{self.initial}\n"
+        output += "#accepting\n"
+        for accept in sorted(self.accept):
+            output += f"{accept}\n"
+        output += "#alphabet\n"
+        for symbol in sorted(self.alphabet):
+            output += f"{symbol}\n"
+        output += "#transitions\n"
+        for trans in self._transitions:
+            output += f"{trans.render()}\n"
+        return output
 
     def _parse_file(self, filename):
         parsingID = operationID.INVALID
@@ -94,23 +155,24 @@ class Automaton():
                     else:
                         # Invalid #operation
                         parsingID = operationID.INVALID
+                        self.errors.append(f"Parsing section not supported: {x}")
                 else:
                     #Parsing states elements
                     if (parsingID == operationID.STATES):
-                        self.states.append(x)
+                        self.states.add(State(x))
                     #Parsing initial elements
                     #you only can have one state of initial
                     elif (parsingID == operationID.INITIAL):
-                        if(self.initial == ""):
+                        if self.initial == "":
                             self.initial = x
                         else:
-                            self.errors.append("Missing initial state")
+                            self.errors.append("More than one initial state")
                     #Parsing accepting elements
                     elif (parsingID == operationID.ACCEPT):
-                        self.accept.append(x)
+                        self.accept.add(x)
                     #Parsing alphabet elements
                     elif (parsingID == operationID.ALPHABET):
-                        self.alphabet.append(x)
+                        self.alphabet.add(x)
                     #Parsing transition elements
                     elif (parsingID == operationID.TRANSITIONS):
                         try:
@@ -119,9 +181,7 @@ class Automaton():
                             a = s[1].split('>')
                             symbol = a[0]
                             for target in a[1].split(','):
-                                self.transitions.append(Transition(origin,target,symbol))
+                                self._transitions.append(Transition(origin, target, symbol))
                         except:
                             self.errors.append(f"Invalid transition definition: {x}")
-                    else:
-                        self.errors.append(f"Parsing section not supported: {x}")
     
--- operations.py
+++ operations.py
@@ -1,13 +1,30 @@
-from automaton import Automaton
+from itertools import product
+
+from automaton import Automaton, State
 
 def union(automaton1: Automaton, automaton2: Automaton) -> Automaton:
-    # TODO: Implement this
-    raise NotImplementedError
+    states = get_states_cross_product(automaton1, automaton2)
+    initial = automaton1.initial + automaton2.initial
+    accept = set()
+    for a1 in automaton1.accept:
+        for s2 in automaton2.get_states_name():
+            accept.add(a1+s2)
+    for a2 in automaton2.accept:
+        for s1 in automaton1.get_states_name():
+            accept.add(s1+a2)
+    alphabet = automaton1.alphabet | automaton2.alphabet
+    return Automaton(states, initial, accept, alphabet)
 
 
 def intersect(automaton1: Automaton, automaton2: Automaton) -> Automaton:
-    # TODO: Implement this
-    raise NotImplementedError
+    states = get_states_cross_product(automaton1, automaton2)
+    initial = automaton1.initial + automaton2.initial
+    accept = set()
+    for a1 in automaton1.accept:
+        for a2 in automaton2.accept:
+            accept.add(a1+a2)
+    alphabet = automaton1.alphabet | automaton2.alphabet
+    return Automaton(states, initial, accept, alphabet)
 
 
 def concat(automaton1: Automaton, automaton2: Automaton) -> Automaton:
@@ -18,3 +35,15 @@ def concat(automaton1: Automaton, automaton2: Automaton) -> Automaton:
 def kleene_star(automaton1: Automaton) -> Automaton:
     # TODO: Implement this
     raise NotImplementedError
+
+def get_states_cross_product(automaton1: Automaton, automaton2: Automaton) -> set:
+    states = set()
+    for s1 in sorted(automaton1.states):
+        for s2 in sorted(automaton2.states):
+            state = State(f"{s1.name}{s2.name}")
+            for trans1 in s1.transitions:
+                for trans2 in s2.transitions:
+                    if trans1.symbol == trans2.symbol:
+                        state.add_transition(f"{trans1.target}{trans2.target}", trans1.symbol)
+            states.add(state)
+    return states
